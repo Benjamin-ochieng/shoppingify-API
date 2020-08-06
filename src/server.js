a1415f73dfcae7679efe3b018b09577d3b75a9c3
Benjamin-ochieng <ochiengbenja@gmail.com>
2020-08-06 12:22:34 +0300

    chore: setup a working server

--- src/server.js
+++ src/server.js
@@ -2,3 +2,18 @@ import express from 'express';
 import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
+
+const app = express();
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
+
+const main = () => {
+  app.listen(3020, () => {
+    console.log('Server started on 3020');
+  });
+};
+
+export default main;
