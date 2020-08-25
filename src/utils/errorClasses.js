f90926c6d941126deaa7e76f3ae1f09b2f59ed42
Benjamin-ochieng <ochiengbenja@gmail.com>
2020-08-25 16:13:15 +0300

    feat(errorClasses): add a Not found error class

---
+++ src/utils/errorClasses.js
@@ -0,0 +1,25 @@
+/* eslint-disable max-classes-per-file */
+const assert = require('assert').strict;
+
+export class ApplicationError extends Error {
+  get name() {
+    return this.constructor.name;
+  }
+
+  get err() {
+    return this;
+  }
+}
+
+export class NotFound extends ApplicationError {
+  constructor(message, options) {
+    super(message);
+    assert(typeof message === 'string');
+    assert(typeof options === 'object');
+    assert(options !== null);
+    for (let i = 0, arr = Object.entries(options); i < arr.length; i += 1) {
+      const [key, value] = arr[i];
+      this[key] = value;
+    }
+  }
+}
